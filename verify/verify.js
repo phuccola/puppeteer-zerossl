a267ed1cb772f4a766166e0ad75828a99e644e08
Nguyễn Phúc <trungphuc94@gmail.com>
2024-08-06 20:50:03 +0700

    update

---
+++ verify/verify.js
@@ -0,0 +1,110 @@
+
+const puppeteer = require('puppeteer')
+const puppeteerExtra = require('puppeteer-extra');
+const Stealth = require('puppeteer-extra-plugin-stealth');
+puppeteerExtra.use(Stealth());
+const fs = require('fs')
+
+const username = 'phucclts_<index>@gmail.com'
+const password = '123456aA'
+const startIndex = 2
+const range = 1
+const endIndex = startIndex + range
+
+async function main () {
+  const resultArray = []
+  const browser = await puppeteer.launch();
+  const array = Array.from({length: range}).map((_, index) => index + startIndex)
+  await Promise.all(array.map((mappedIndex)=> getHash(browser, mappedIndex, resultArray)))
+  console.log(resultArray)
+  // writeToFile(resultArray)
+  browser.close()
+}
+
+async function getHash(browser, index, resultArray) {
+  const page = await browser.newPage();
+  await page.setViewport({width: 1254, height: 1024});
+  await page.setDefaultTimeout(5000);
+  await page.goto('https://app.zerossl.com/login');
+  await page.type('#login input[type=email]', username.replace('<index>', index));
+  await page.type('#login input[type=password]', password);
+
+  try {
+    const loginBtnSelector = await page.waitForSelector('#login > fieldset > div:nth-child(5) > button');
+    await loginBtnSelector.click();
+  } catch (error) {
+    console.log('loginBtnSelector')
+    console.log(error)
+  }
+
+  try {
+    await page.waitForNavigation();
+  } catch (error) {
+    console.log('waitForNavigation')
+    console.log(error)
+  }
+
+  try {
+    const draftBtnSelector = await page.waitForSelector('body > div:nth-child(5) > section > div > main > div > div > section.tabs_section > div > a:nth-child(1)');
+    await draftBtnSelector.click();
+  } catch (error) {
+    console.log('draftBtnSelector')
+    console.log(error)
+  }
+
+  try {
+    await page.waitForNetworkIdle();
+  } catch (error) {
+    console.log('waitForNetworkIdle')
+    console.log(error)
+  }
+
+  try {
+    const verifyBtnSelector = await page.waitForSelector('body > div:nth-child(5) > section > div > main > div > div > section.list > ul > li:nth-child(2) > div.actions.dropdown > a.button.primary');
+    await verifyBtnSelector.click({
+      offset: {
+        x: 92,
+        y: 0,
+      }
+    });
+  } catch (error) {
+    await page.screenshot({path: `screenshot/verifyBtnSelector_${index}.png`})
+    console.log('verifyBtnSelector')
+    console.log(error)
+  }
+
+  try {
+    const copyBtnSelector = await page.waitForSelector('body > div:nth-child(5) > section > div > main > div > div > section.list > ul > li:nth-child(2) > div.actions.dropdown > ul > li:nth-child(1) > a');
+    await copyBtnSelector.click();
+    const hash = await page.evaluate(() => navigator.clipboard.readText())
+    await page.screenshot({path: `screenshot/copyBtnSelector_${index}.png`})
+    console.log(hash)
+  } catch (error) {
+    await page.screenshot({path: `screenshot/copyBtnSelector_${index}.png`})
+    console.log('copyBtnSelector')
+    console.log(error)
+  }
+
+  // try {
+  //   const developerSelector = await page.waitForSelector('body > div:nth-child(5) > section > div > div.left > div > ul > li.developer > a');
+  //   developerSelector.click()
+  // } catch (error) {
+  //   console.log('developerSelector')
+  //   console.log(error)
+  //   await page.screenshot({path: `screenshot/developerSelector_${index}.png`})
+  // }
+
+  // try {
+  //   const apiKeySelector = await page.waitForSelector('body > div:nth-child(5) > section > div > main > div > div > section:nth-child(2) > div:nth-child(2) > div > span');
+  //   const APIKey = await  apiKeySelector?.evaluate(el => el.textContent)
+  //   resultArray.push([username.replace('<index>', index), APIKey]);
+  // } catch (error) {
+  //   console.log('apiKeySelector')
+  //   console.log(error)
+  //   await page.screenshot({path: `screenshot/apiKeySelector_${index}.png`})
+  // }
+
+  await page.close();
+}
+
+main()
